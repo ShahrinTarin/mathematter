6a165c2cd894855d075aafe40e18ed879ce02c04
ShahrinTarin <rahmanshahrintarin@gmail.com>
2025-06-13 00:53:26 +0600

    wishlist backend done

--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,15 +1,29 @@
-import React, { use } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import { AuthContext } from '../Provider/AuthProvider';
+import axios from 'axios';
 
 
 const Wishlist = () => {
-    const {user}=use(AuthContext)
-    
-    return (
-        <div className='min-h-[calc(100vh-64px)] w-11/12 mx-auto'>
-            wishlist
-        </div>
-    );
+    const { user } = use(AuthContext)
+    const [wishlists, setwishlists] = useState([])
+ useEffect(() => {
+   
+    axios(`http://localhost:3000/wishlist/${user?.email}`)
+      .then(data => {
+        console.log(data?.data)
+        setwishlists(data?.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [user])
+return (
+    <div className='min-h-[calc(100vh-64px)] w-11/12 mx-auto'>
+       {
+       wishlists.map(wishlist=><p>oijhgg</p>)
+       }
+    </div>
+);
 };
 
 export default Wishlist;
