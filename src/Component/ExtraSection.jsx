f62851f97d013192805a9bbc04d5c7320553dbf5
ShahrinTarin <rahmanshahrintarin@gmail.com>
2025-06-16 11:48:25 +0600

    all done

--- src/Component/ExtraSection.jsx
+++ src/Component/ExtraSection.jsx
@@ -133,7 +133,7 @@ const ExtraSection = () => {
                         </p>
                     </div>
                 </div>
-                <Link to='/allblogs' className="mt-5 cursor-pointer px-5 py-2.5 lg:mt-5 relative rounded group text-white font-medium inline-block">
+                <Link to='/allblogs' className="mt-8 cursor-pointer px-5 py-2.5 lg:mt-5 relative rounded group text-white font-medium inline-block">
                     <span className="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-gray-600 to-[#1b9c85]"></span>
                     <span className="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-gray-600 to-[#1b9c85]"></span>
                     <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-gray-600 to-[#1b9c85]"></span>
