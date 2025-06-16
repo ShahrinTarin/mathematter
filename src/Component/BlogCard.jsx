80026039c66e852258e5447c4923b9cb9079f0e0
ShahrinTarin <rahmanshahrintarin@gmail.com>
2025-06-16 07:56:06 +0600

    kisu baki

--- src/Component/BlogCard.jsx
+++ src/Component/BlogCard.jsx
@@ -41,7 +41,7 @@ const BlogCard = ({ blog }) => {
     }
     return (
       <Zoom delay={200}>
-        <div className="relative flex  flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+        <div className="relative flex h-full  flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
             <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
                 <img className='h-full w-full' src={newblog.image} alt="" />
                 <StyledWrapper>
@@ -53,7 +53,7 @@ const BlogCard = ({ blog }) => {
             </div>
 
             <div className="p-6">
-                <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                <h5 className="mb-2 font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
                     {newblog.title}
                 </h5>
                 <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
