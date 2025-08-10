d7756901317392825736cdbcc9bfa9077c1db3db
ShahrinTarin <rahmanshahrintarin@gmail.com>
2025-08-10 13:27:29 +0600

    okay

--- src/Pages/AllBlogs.jsx
+++ src/Pages/AllBlogs.jsx
@@ -5,26 +5,31 @@ import Loader from '../Component/Loader';
 import axios from 'axios';
 
 const AllBlogs = () => {
-    const {loading}=use(AuthContext)
+    const { loading } = use(AuthContext)
     const [initialBlogs, setInitialBlogs] = useState([])
     const [search, setSearch] = useState('')
     const [searchTitle, setSearchTitle] = useState('')
     useEffect(() => {
         axios(`https://assignment-11-server-two-drab.vercel.app/blogs?category=${search}&title=${searchTitle}`)
             .then(data => setInitialBlogs(data.data))
-    }, [search,searchTitle])
-    if ( loading) {
+    }, [search, searchTitle])
+    if (loading) {
         return <Loader></Loader>
     }
 
     return (
         <div className=' min-h-[calc(100vh-64px)]'>
             <div>
-                <div className='lg:w-9/12 pt-12 w-11/12 px-10 mx-auto space-y-4 '>
-                    <label className="input w-full">
-                        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <div className='lg:w-9/12 pt-12 w-11/12 px-10 mx-auto space-y-4'>
+                    <label className="input w-full bg-white text-gray-800 dark:bg-black dark:text-white">
+                        <svg
+                            className="h-[1em] opacity-70 text-gray-600 dark:text-gray-300"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                        >
                             <g
                                 strokeLinejoin="round"
+
                                 strokeLinecap="round"
                                 strokeWidth="2.5"
                                 fill="none"
@@ -34,11 +39,20 @@ const AllBlogs = () => {
                                 <path d="m21 21-4.3-4.3"></path>
                             </g>
                         </svg>
-                        <input onChange={(e) => setSearchTitle(e.target.value)} type="search" className="grow" placeholder="Search by Title" />
-                        <kbd className="kbd kbd-sm">⌘</kbd>
-                        <kbd className="kbd kbd-sm">K</kbd>
+
+                        <input
+                            onChange={(e) => setSearchTitle(e.target.value)}
+                            type="search"
+                            className="grow bg-transparent outline-none text-gray-800 dark:text-white placeholder-gray-500 dark:placeholder-gray-400"
+                            placeholder="Search by Title"
+                        />
+
+                        <kbd className="kbd text-white bg-gray-600 dark:bg-black   dark:text-white kbd-sm">⌘</kbd>
+                        <kbd className="kbd text-white dark:bg-black  bg-gray-500 dark:text-white kbd-sm">K</kbd>
                     </label>
                 </div>
+
+
                 <div className='pt-12 md:flex justify-between lg:w-9/12 w-11/12  mx-auto space-y-4 '>
                     <h1 className=" text-3xl lg:text-5xl pl:12 font-semibold text-gray-700 dark:text-gray-400">All <span className='text-[#1b9c85]'>blogs Here</span></h1>
                     <div>
